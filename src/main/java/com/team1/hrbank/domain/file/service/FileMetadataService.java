39d5bfe6db834d5766231edae94838b9f7fa1705
joongwon <anjoongwon517@gmail.com>
2025-07-30 18:51:43 +0900

    refactor: 불필요한 인터페이스 제거

--- src/main/java/com/team1/hrbank/domain/file/service/FileMetadataService.java
+++
@@ -1,9 +0,0 @@
-package com.team1.hrbank.domain.file.service;
-
-import com.team1.hrbank.domain.file.dto.FileMetadataDto;
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileMetadataService {
-
-  FileMetadataDto uploadProfile(MultipartFile file);
-}
