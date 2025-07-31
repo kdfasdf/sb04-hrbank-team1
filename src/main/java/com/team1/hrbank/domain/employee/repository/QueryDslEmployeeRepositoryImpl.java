e14666edde0fd86cd87fb3a16ea01305e8c6bdcc
KwonYongJin <chaoskyj@gmail.com>
2025-07-31 21:55:34 +0900

    feat: 사용자 정보별로 검색하는 기능을 QueryDSL을 통해 구현 및 QueryDSL을 위한 의존성 추가

---
+++ src/main/java/com/team1/hrbank/domain/employee/repository/QueryDslEmployeeRepositoryImpl.java
@@ -0,0 +1,80 @@
+package com.team1.hrbank.domain.employee.repository;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.team1.hrbank.domain.employee.dto.request.CursorPageRequestDto;
+import com.team1.hrbank.domain.employee.entity.Employee;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+
+public class QueryDslEmployeeRepositoryImpl implements QueryDslEmployeeRepository {
+
+  private final JPAQueryFactory queryFactory;
+
+  public QueryDslEmployeeRepositoryImpl(EntityManager em) {
+    this.queryFactory = new JPAQueryFactory(em);
+  }
+
+  @Override
+  public List<Employee> findAllEmployeesByRequest(CursorPageRequestDto cursorPageRequestDto) {
+
+    QEmployee employee = QEmployee.employee;
+
+    String sortField = cursorPageRequestDto.sortField();
+    String sortDirection = cursorPageRequestDto.sortDirection();
+
+    String nameOrEmail = cursorPageRequestDto.nameOrEmail();       // nullable
+    String departmentName = cursorPageRequestDto.departmentName(); // nullable
+    String position = cursorPageRequestDto.position();             // nullable
+    String status = cursorPageRequestDto.status();                 // nullable, String or Enum?
+
+    if (nameOrEmail == null) {
+      nameOrEmail = "";
+    }
+    if (departmentName == null) {
+      departmentName = "";
+    }
+    if (position == null) {
+      position = "";
+    }
+    if (status == null) {
+      status = "";
+    }
+
+    Order order = Order.DESC; // 기본 DESC
+    if ("ASC".equalsIgnoreCase(sortDirection)) {
+      order = Order.ASC;
+    }
+
+    // 조건들 조합
+    BooleanBuilder where = new BooleanBuilder();
+    if (!nameOrEmail.isBlank()) {
+      where.and(
+          employee.name.containsIgnoreCase(nameOrEmail)
+              .or(employee.email.containsIgnoreCase(nameOrEmail))
+      );
+    }
+    if (!position.isBlank()) {
+      where.and(employee.position.containsIgnoreCase(position));
+    }
+    if (!status.isBlank()) {
+      where.and(employee.status.equalsIgnoreCase(status));
+    }
+    if (!departmentName.isBlank()) {
+      where.and(employee.department.name.containsIgnoreCase(departmentName));
+    }
+
+    // 쿼리 실행
+    List<Employee> employees = queryFactory
+        .selectFrom(employee)
+        .where(where)
+        .orderBy(order)
+        .fetch();
+
+    return employees;
+  }
+
+}
