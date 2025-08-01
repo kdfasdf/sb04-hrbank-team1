0ff0885fdda1f0eaf10423f4c7b341b1e1ae67bd
hello13580 <jhc5280@naver.com>
2025-08-01 18:45:18 +0900

    refactor: ansi 표준 쿼리로 ChangeLog 리포지토리와 서비스 수정

--- src/main/java/com/team1/hrbank/domain/changelog/controller/ChangeLogDiffController.java
+++ src/main/java/com/team1/hrbank/domain/changelog/controller/ChangeLogDiffController.java
@@ -1,4 +1,26 @@
 package com.team1.hrbank.domain.changelog.controller;
 
+import com.team1.hrbank.domain.changelog.dto.data.ChangeLogDiffDto;
+import com.team1.hrbank.domain.changelog.service.ChangeLogDiffService;
+import com.team1.hrbank.domain.changelog.service.ChangeLogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/change-logs")
 public class ChangeLogDiffController {
+    private final ChangeLogDiffService changeLogDiffService;
+
+    @GetMapping("/{changeLogId}/diffs")
+    public ResponseEntity<List<ChangeLogDiffDto>> getDiffs(@PathVariable Long changeLogId) {
+        List<ChangeLogDiffDto> diffs = changeLogDiffService.findAllByChangeLogId(changeLogId);
+        return ResponseEntity.ok(diffs);
+    }
 }
