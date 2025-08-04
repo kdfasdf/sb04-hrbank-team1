82bdad0236667a4d275c7ec659ec82ea0b8b3843
Eunhye0k <dmsgur7305@naver.com>
2025-08-04 16:37:51 +0900

    chore: 사용하지 않는 import문 삭제

--- src/main/java/com/team1/hrbank/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/team1/hrbank/global/error/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.team1.hrbank.global.error;
 
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
