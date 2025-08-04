f9d11ca0b67c455fcfc159e93f0bbb10e0f77d13
kdfasdf <96770726+kdfasdf@users.noreply.github.com>
2025-08-04 16:21:48 +0900

    fix : 백업 엔티티 필드 불일치 문제 해결 - startedAt, createdAt 매칭 - backup createdAt,backupDto startedAt 매칭

--- src/main/java/com/team1/hrbank/domain/backup/mapper/BackupMapper.java
+++ src/main/java/com/team1/hrbank/domain/backup/mapper/BackupMapper.java
@@ -3,9 +3,12 @@ package com.team1.hrbank.domain.backup.mapper;
 import com.team1.hrbank.domain.backup.dto.response.BackupDto;
 import com.team1.hrbank.domain.backup.entity.Backup;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface BackupMapper {
 
+
+  @Mapping(target = "startedAt", source = "createdAt")
   BackupDto toDto(Backup backup);
 }
