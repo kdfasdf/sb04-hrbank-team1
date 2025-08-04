6c84f929e7d2b77373aad2aaa61fe54b76c33fd0
KwonYongJin <chaoskyj@gmail.com>
2025-08-04 15:31:50 +0900

    fix: http상태 코드를 하드코딩하지 않고 HttpStatus를 사용하도록 변경

--- src/main/java/com/team1/hrbank/domain/employee/constant/EmployeeErrorCode.java
+++ src/main/java/com/team1/hrbank/domain/employee/constant/EmployeeErrorCode.java
@@ -3,13 +3,14 @@ package com.team1.hrbank.domain.employee.constant;
 import com.team1.hrbank.global.constant.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum EmployeeErrorCode implements ErrorCode {
 
-  EMPLOYEE_NOT_FOUND(404, "찾을 수 없습니다.", "해당 직원을 찾을 수 없습니다."),
-  EMPLOYEE_EMAIL_DUPLICATE(409, "중복된 이메일 입니다.", "해당 이메일은 중복된 이메일입니다.");
+  EMPLOYEE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "찾을 수 없습니다.", "해당 직원을 찾을 수 없습니다."),
+  EMPLOYEE_EMAIL_DUPLICATE(HttpStatus.CONFLICT.value(), "중복된 이메일 입니다.", "해당 이메일은 중복된 이메일입니다.");
 
   private final int status;
   private final String message;
