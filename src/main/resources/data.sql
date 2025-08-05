e51b968c7338c2710a8557d07d1158ba3f0d30b5
joongwon <anjoongwon517@gmail.com>
2025-08-05 11:55:53 +0900

    fix: sql문 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,137 +1,148 @@
--- departments
+-- -- departments
 INSERT INTO departments (id, name, description, established_date, created_at, updated_at)
-VALUES (1, '인사팀', '인사 및 채용 담당', '2010-03-01', '2025-07-06 12:00:00', '2025-07-06 12:00:00');
+VALUES (101, '인사팀', '인사 및 채용 담당', '2010-03-01', '2025-07-06 12:00:00', '2025-07-06 12:00:00');
 INSERT INTO departments (id, name, description, established_date, created_at, updated_at)
-VALUES (2, '재무팀', '회계 및 자금 관리', '2011-07-15', '2025-07-06 12:00:00', '2025-07-06 12:00:00');
+VALUES (102, '재무팀', '회계 및 자금 관리', '2011-07-15', '2025-07-06 12:00:00', '2025-07-06 12:00:00');
 INSERT INTO departments (id, name, description, established_date, created_at, updated_at)
-VALUES (3, '개발팀', '소프트웨어 개발 및 유지보수', '2015-01-10', '2025-07-06 12:00:00', '2025-07-06 12:00:00');
+VALUES (103, '개발팀', '소프트웨어 개발 및 유지보수', '2015-01-10', '2025-07-06 12:00:00', '2025-07-06 12:00:00');
 INSERT INTO departments (id, name, description, established_date, created_at, updated_at)
-VALUES (4, '마케팅팀', '시장 조사 및 광고 전략', '2013-05-22', '2025-07-06 12:00:00', '2025-07-06 12:00:00');
+VALUES (104, '마케팅팀', '시장 조사 및 광고 전략', '2013-05-22', '2025-07-06 12:00:00', '2025-07-06 12:00:00');
 INSERT INTO departments (id, name, description, established_date, created_at, updated_at)
-VALUES (5, '운영팀', '일반 운영 및 관리', '2012-09-30', '2025-07-06 12:00:00', '2025-07-06 12:00:00');
+VALUES (105, '운영팀', '일반 운영 및 관리', '2012-09-30', '2025-07-06 12:00:00', '2025-07-06 12:00:00');
 
--- employees (11명)
+-- -- employees (11명)
 INSERT INTO employees (id, employee_number, name, email, position, hire_date, status, department_id,
                        created_at, updated_at)
-VALUES (1, 'EMP001', '김철수', 'chulsoo.kim@example.com', 'Manager', '2022-01-15', 'ACTIVE', 1, NOW(),
+VALUES (101, 'EMP001', '김철수', 'chulsoo.kim@example.com', 'Manager', '2022-01-15', 'ACTIVE', 101, NOW(),
         NOW()),
-       (2, 'EMP002', '이영희', 'younghee.lee@example.com', 'Staff', '2021-12-10', 'ON_LEAVE', 2, NOW(),
+       (102, 'EMP002', '이영희', 'younghee.lee@example.com', 'Staff', '2021-12-10', 'ON_LEAVE', 102, NOW(),
         NOW()),
-       (3, 'EMP003', '박민수', 'minsoo.park@example.com', 'Developer', '2023-03-08', 'RESIGNED', 3,
+       (103, 'EMP003', '박민수', 'minsoo.park@example.com', 'Developer', '2023-03-08', 'RESIGNED', 103,
         NOW(), NOW()),
-       (4, 'EMP004', '최지훈', 'jihoon.choi@example.com', 'Manager', '2020-08-25', 'ACTIVE', 4, NOW(),
+       (104, 'EMP004', '최지훈', 'jihoon.choi@example.com', 'Manager', '2020-08-25', 'ACTIVE', 104, NOW(),
         NOW()),
-       (5, 'EMP005', '정다은', 'daeun.jung@example.com', 'Staff', '2023-01-01', 'ON_LEAVE', 5, NOW(),
+       (105, 'EMP005', '정다은', 'daeun.jung@example.com', 'Staff', '2023-01-01', 'ON_LEAVE', 105, NOW(),
         NOW()),
-       (6, 'EMP006', '한지민', 'jimin.han@example.com', 'Developer', '2022-04-30', 'ACTIVE', 1, NOW(),
+       (106, 'EMP006', '한지민', 'jimin.han@example.com', 'Developer', '2022-04-30', 'ACTIVE', 101, NOW(),
         NOW()),
-       (7, 'EMP007', '오세훈', 'sehoon.oh@example.com', 'Manager', '2019-09-14', 'RESIGNED', 2, NOW(),
+       (107, 'EMP007', '오세훈', 'sehoon.oh@example.com', 'Manager', '2019-09-14', 'RESIGNED', 102, NOW(),
         NOW()),
-       (8, 'EMP008', '장서연', 'seoyeon.jang@example.com', 'Staff', '2021-10-05', 'ACTIVE', 3, NOW(),
+       (108, 'EMP008', '장서연', 'seoyeon.jang@example.com', 'Staff', '2021-10-05', 'ACTIVE', 103, NOW(),
         NOW()),
-       (9, 'EMP009', '배지훈', 'jihoon.bae@example.com', 'Developer', '2020-11-20', 'ON_LEAVE', 4,
+       (109, 'EMP009', '배지훈', 'jihoon.bae@example.com', 'Developer', '2020-11-20', 'ON_LEAVE', 104,
         NOW(), NOW()),
-       (10, 'EMP010', '송지수', 'jisu.song@example.com', 'Staff', '2023-06-01', 'ACTIVE', 5, NOW(),
+       (110, 'EMP010', '송지수', 'jisu.song@example.com', 'Staff', '2023-06-01', 'ACTIVE', 105, NOW(),
         NOW()),
-       (11, 'EMP050', '유하늘', 'haneul.yoo@example.com', 'Manager', '2024-05-03', 'RESIGNED', 3,
+       (111, 'EMP050', '유하늘', 'haneul.yoo@example.com', 'Manager', '2024-05-03', 'RESIGNED', 103,
         NOW(), NOW());
 
 
--- file_metadatas (profile images)
+-- -- file_metadatas
 INSERT INTO file_metadatas (id, original_name, saved_name, extension, type, size, path, created_at,
                             updated_at)
-VALUES (1, 'chulsoo.png', 'profile_1_3a2f7c98-4b2a-4fdd-9b29-bcbd1234abcd.png', 'png', 'PROFILE',
-        234512, 'uploads/profile/profile_1_3a2f7c98-4b2a-4fdd-9b29-bcbd1234abcd.png', NOW(), NOW()),
-       (2, 'younghee.jpg', 'profile_2_7e15b7d2-9f44-4567-8264-f14d5678dcba.jpg', 'jpg', 'PROFILE',
-        185233, 'uploads/profile/profile_2_7e15b7d2-9f44-4567-8264-f14d5678dcba.jpg', NOW(), NOW()),
-       (3, 'minsoo.jpeg', 'profile_3_ba18f7a4-139b-40df-a5fa-abcde9876543.jpeg', 'jpeg', 'PROFILE',
-        279844, 'uploads/profile/profile_3_ba18f7a4-139b-40df-a5fa-abcde9876543.jpeg', NOW(),
-        NOW()),
-       (4, 'jihoon.jpg', 'profile_4_9a4dfc91-32c3-4d4e-9f7c-c1c2344def12.jpg', 'jpg', 'PROFILE',
-        198745, 'uploads/profile/profile_4_9a4dfc91-32c3-4d4e-9f7c-c1c2344def12.jpg', NOW(), NOW()),
-       (5, 'daeun.png', 'profile_5_e3a47aa3-6ab1-44df-bb99-cc7766acb711.png', 'png', 'PROFILE',
-        300152, 'uploads/profile/profile_5_e3a47aa3-6ab1-44df-bb99-cc7766acb711.png', NOW(), NOW()),
-       (6, 'jimin.jpeg', 'profile_6_44b5f3d2-6f78-4e1e-98b0-aaa222be7788.jpeg', 'jpeg', 'PROFILE',
-        412333, 'uploads/profile/profile_6_44b5f3d2-6f78-4e1e-98b0-aaa222be7788.jpeg', NOW(),
-        NOW()),
-       (7, 'sehoon.jpg', 'profile_7_8fcba27c-1111-4a9d-b220-556688abc123.jpg', 'jpg', 'PROFILE',
-        255123, 'uploads/profile/profile_7_8fcba27c-1111-4a9d-b220-556688abc123.jpg', NOW(), NOW()),
-       (8, 'seoyeon.png', 'profile_8_91b224ee-22e2-4dfd-82e2-fbd1119988de.png', 'png', 'PROFILE',
-        199232, 'uploads/profile/profile_8_91b224ee-22e2-4dfd-82e2-fbd1119988de.png', NOW(), NOW()),
-       (9, 'jihoon2.jpg', 'profile_9_3cffa011-a44a-4a1c-a12e-bddc2222be34.jpg', 'jpg', 'PROFILE',
-        300324, 'uploads/profile/profile_9_3cffa011-a44a-4a1c-a12e-bddc2222be34.jpg', NOW(), NOW()),
-       (10, 'jisu.jpeg', 'profile_10_5ddf3a89-5567-49b3-92dd-ccbb33ff4499.jpeg', 'jpeg', 'PROFILE',
-        278234, 'uploads/profile/profile_10_5ddf3a89-5567-49b3-92dd-ccbb33ff4499.jpeg', NOW(),
-        NOW());
-
--- change_logs (10개)
+VALUES (101, 'file1.csv', 'file1_20250805.csv', 'csv', 'BACKUP', 1024, '/files/file1_20250805.csv',
+        NOW(), NOW()),
+       (102, 'file2.csv', 'file2_20250805.csv', 'csv', 'BACKUP', 2048, '/files/file2_20250805.csv',
+        NOW(), NOW()),
+       (103, 'file3.csv', 'file3_20250805.csv', 'csv', 'BACKUP', 4096, '/files/file3_20250805.csv',
+        NOW(), NOW()),
+       (104, 'file2.log', 'file4_20250805.log', 'log', 'ERROR_LOG', 2048, '/files/file4_20250805.log',
+        NOW(), NOW()),
+       (105, 'file5.csv', 'file5_20250805.csv', 'csv', 'BACKUP', 4096, '/files/file5_20250805.csv',
+        NOW(), NOW()),
+       (106, 'file1.csv', 'file1_20250805.csv', 'csv', 'BACKUP', 1024, '/files/file6_20250805.csv',
+        NOW(), NOW()),
+       (107, 'file2.csv', 'file2_20250805.csv', 'csv', 'BACKUP', 2048, '/files/file7_20250805.csv',
+        NOW(), NOW()),
+       (108, 'file3.csv', 'file3_20250805.csv', 'csv', 'BACKUP', 4096, '/files/file8_20250805.csv',
+        NOW(), NOW()),
+       (109, 'file2.log', 'file4_20250805.log', 'log', 'ERROR_LOG', 2048, '/files/file9_20250805.log',
+        NOW(), NOW()),
+       (110, 'file5.csv', 'file5_20250805.csv', 'csv', 'BACKUP', 4096, '/files/file10_20250805.csv',
+        NOW(), NOW()),
+       (111, 'file2.csv', 'file2_20250805.csv', 'csv', 'BACKUP', 2048, '/files/file11_20250805.csv',
+        NOW(), NOW()),
+       (112, 'file5.csv', 'file5_20250805.csv', 'csv', 'BACKUP', 4096, '/files/file12_20250805.csv',
+        NOW(), NOW()),
+       (113, 'file3.csv', 'file3_20250805.csv', 'csv', 'BACKUP', 4096, '/files/file13_20250805.csv',
+        NOW(), NOW()),
+       (114, 'file2.log', 'file4_20250805.log', 'log', 'ERROR_LOG', 2048, '/files/file14_20250805.log',
+        NOW(), NOW()),
+       (115, 'file5.csv', 'file5_20250805.csv', 'csv', 'BACKUP', 4096, '/files/file15_20250805.csv',
+        NOW(), NOW()),
+       (116, 'file1.csv', 'file1_20250805.csv', 'csv', 'BACKUP', 1024, '/files/file16_20250805.csv',
+        NOW(), NOW()),
+       (117, 'file2.csv', 'file2_20250805.csv', 'csv', 'BACKUP', 2048, '/files/file17_20250805.csv',
+        NOW(), NOW()),
+       (118, 'file3.csv', 'file3_20250805.csv', 'csv', 'BACKUP', 4096, '/files/file18_20250805.csv',
+        NOW(), NOW()),
+       (119, 'file2.log', 'file4_20250805.log', 'log', 'ERROR_LOG', 2048, '/files/file19_20250805.log',
+        NOW(), NOW()),
+       (120, 'file20.csv', 'file20_20250805.csv', 'csv', 'BACKUP', 5120,
+        '/files/file20_20250805.csv', NOW(), NOW());
+-- -- change_logs (10개)
 INSERT INTO change_logs (id, employee_number, type, memo, ip_address, created_at, updated_at)
-VALUES (1, 'EMP001', 'CREATED', '신규 입사 등록', '192.168.0.11', NOW(), NOW()),
-       (2, 'EMP002', 'UPDATED', '이메일 주소 수정', '192.168.0.12', NOW(), NOW()),
-       (3, 'EMP003', 'UPDATED', '부서 이동 처리', '10.0.0.1', NOW(), NOW()),
-       (4, 'EMP004', 'DELETED', '퇴사 처리', '172.16.1.1', NOW(), NOW()),
-       (5, 'EMP005', 'CREATED', '직책 신규 등록', '192.168.1.20', NOW(), NOW()),
-       (6, 'EMP006', 'UPDATED', '프로필 이미지 변경', '10.10.10.10', NOW(), NOW()),
-       (7, 'EMP007', 'UPDATED', '메모 업데이트', '192.168.100.100', NOW(), NOW()),
-       (8, 'EMP008', 'DELETED', '계정 비활성화', '203.0.113.5', NOW(), NOW()),
-       (9, 'EMP010', 'CREATED', '이력 정보 등록', '198.51.100.22', NOW(), NOW()),
-       (10, 'EMP010', 'UPDATED', '직책 수정', '172.31.255.254', NOW(), NOW());
+VALUES (101, 'EMP001', 'CREATED', '신규 입사 등록', '192.168.0.11', NOW(), NOW()),
+       (102, 'EMP002', 'UPDATED', '이메일 주소 수정', '192.168.0.12', NOW(), NOW()),
+       (103, 'EMP003', 'UPDATED', '부서 이동 처리', '10.0.0.1', NOW(), NOW()),
+       (104, 'EMP004', 'DELETED', '퇴사 처리', '172.16.1.1', NOW(), NOW()),
+       (105, 'EMP005', 'CREATED', '직책 신규 등록', '192.168.1.20', NOW(), NOW()),
+       (106, 'EMP006', 'UPDATED', '프로필 이미지 변경', '10.10.10.10', NOW(), NOW()),
+       (107, 'EMP007', 'UPDATED', '메모 업데이트', '192.168.100.100', NOW(), NOW()),
+       (108, 'EMP008', 'DELETED', '계정 비활성화', '203.0.113.5', NOW(), NOW()),
+       (109, 'EMP010', 'CREATED', '이력 정보 등록', '198.51.100.22', NOW(), NOW()),
+       (110, 'EMP010', 'UPDATED', '직책 수정', '172.31.255.254', NOW(), NOW());
 
--- change_log_diffs
-INSERT INTO change_log_diffs (id, change_log_id, property_name, before_value, after_value)
-VALUES (1, 1, 'position', '', 'Manager'),                                        -- CREATED
-       (2, 2, 'email', 'younghee.lee@example.com', 'younghee_2024@example.com'), -- UPDATED
-       (3, 2, 'status', 'ON_LEAVE', 'ACTIVE'),
-       (4, 3, 'department_id', '3', '2'),                                        -- UPDATED
-       (5, 4, 'status', 'ACTIVE', 'RESIGNED'),                                   -- DELETED
-       (6, 5, 'position', '', 'Staff'),                                          -- CREATED
-       (7, 6, 'position', 'Developer', 'Senior Developer'),                      -- UPDATED
-       (8, 7, 'memo', '열심히 일함', '프로젝트 리더 승격'),                                   -- UPDATED
-       (9, 8, 'status', 'ACTIVE', 'RESIGNED'),                                   -- DELETED
-       (10, 9, 'hire_date', '', '2023-06-01'),                                   -- CREATED
-       (11, 9, 'name', '', '송지수'),
-       (12, 10, 'position', 'Staff', 'Team Lead'),                               -- UPDATED
-       (13, 10, 'email', 'jisu.song@example.com', 'jisu.song@newdomain.com'),
-       (14, 6, 'memo', '기존 메모 없음', '성실함 강조'),                                    -- UPDATED
-       (15, 3, 'position', 'Developer', 'Manager');
--- UPDATED
+-- -- change_log_diffs
+INSERT INTO change_log_diffs (id, change_log_id, property_name, before_value, after_value,
+                              created_at, updated_at)
+VALUES (101, 101, 'CREATED', NULL, '신규 입사 등록', NOW(), NOW()),
+       (102, 102, 'UPDATED', '이메일 주소 이전 값', '이메일 주소 수정 후 값', NOW(), NOW()),
+       (103, 103, 'UPDATED', '이전 부서', '이동된 부서', NOW(), NOW()),
+       (104, 104, 'DELETED', '재직 중', '퇴사 처리됨', NOW(), NOW()),
+       (105, 105, 'CREATED', NULL, '직책 신규 등록', NOW(), NOW()),
+       (106, 106, 'UPDATED', '기존 프로필 이미지', '새 프로필 이미지', NOW(), NOW()),
+       (107, 107, 'UPDATED', '이전 메모', '업데이트된 메모', NOW(), NOW()),
+       (108, 108, 'DELETED', '계정 활성 상태', '계정 비활성화 처리', NOW(), NOW()),
+       (109, 109, 'CREATED', NULL, '이력 정보 신규 등록', NOW(), NOW()),
+       (110, 110, 'UPDATED', '이전 직책', '수정된 직책', NOW(), NOW());
 
 -- backups
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (2, '2025-07-18T00:00:00', '2025-07-18T00:03:00', 2, 'IN_PROGRESS', '192.168.0.2', NOW());
+VALUES (101, '2025-07-18T00:00:00', '2025-07-18T00:03:00', 102, 'IN_PROGRESS', '192.168.0.2', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (3, '2025-07-19T00:00:00', '2025-07-19T00:04:00', 3, 'IN_PROGRESS', '192.168.0.3', NOW());
+VALUES (103, '2025-07-19T00:00:00', '2025-07-19T00:04:00', 103, 'IN_PROGRESS', '192.168.0.3', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (4, '2025-07-20T00:00:00', '2025-07-20T00:02:00', 4, 'SKIPPED', '192.168.0.4', NOW());
+VALUES (104, '2025-07-20T00:00:00', '2025-07-20T00:02:00', 104, 'SKIPPED', '192.168.0.4', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (5, '2025-07-21T00:00:00', '2025-07-21T00:05:00', 5, 'FAIL', '192.168.0.5', NOW());
+VALUES (105, '2025-07-21T00:00:00', '2025-07-21T00:05:00', 105, 'FAIL', '192.168.0.5', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (6, '2025-07-22T00:00:00', '2025-07-22T00:02:00', 6, 'IN_PROGRESS', '192.168.0.6', NOW());
+VALUES (106, '2025-07-22T00:00:00', '2025-07-22T00:02:00', 106, 'IN_PROGRESS', '192.168.0.6', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (7, '2025-07-23T00:00:00', '2025-07-23T00:05:00', 7, 'COMPLETED', '192.168.0.7', NOW());
+VALUES (107, '2025-07-23T00:00:00', '2025-07-23T00:05:00', 107, 'COMPLETED', '192.168.0.7', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (8, '2025-07-24T00:00:00', '2025-07-24T00:03:00', 8, 'COMPLETED', '192.168.0.8', NOW());
+VALUES (108, '2025-07-24T00:00:00', '2025-07-24T00:03:00', 108, 'COMPLETED', '192.168.0.8', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (9, '2025-07-25T00:00:00', '2025-07-25T00:05:00', 9, 'IN_PROGRESS', '192.168.0.9', NOW());
+VALUES (109, '2025-07-25T00:00:00', '2025-07-25T00:05:00', 109, 'IN_PROGRESS', '192.168.0.9', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (10, '2025-07-26T00:00:00', '2025-07-26T00:05:00', 10, 'FAIL', '192.168.0.10', NOW());
+VALUES (110, '2025-07-26T00:00:00', '2025-07-26T00:05:00', 110, 'FAIL', '192.168.0.10', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (11, '2025-07-27T00:00:00', '2025-07-27T00:02:00', 11, 'FAIL', '192.168.0.11', NOW());
+VALUES (111, '2025-07-27T00:00:00', '2025-07-27T00:02:00', 111, 'FAIL', '192.168.0.11', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (12, '2025-07-28T00:00:00', '2025-07-28T00:02:00', 12, 'COMPLETED', '192.168.0.12', NOW());
+VALUES (112, '2025-07-28T00:00:00', '2025-07-28T00:02:00', 112, 'COMPLETED', '192.168.0.12', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (13, '2025-07-29T00:00:00', '2025-07-29T00:05:00', 13, 'SKIPPED', '192.168.0.13', NOW());
+VALUES (113, '2025-07-29T00:00:00', '2025-07-29T00:05:00', 113, 'SKIPPED', '192.168.0.13', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (14, '2025-07-30T00:00:00', '2025-07-30T00:05:00', 14, 'FAIL', '192.168.0.14', NOW());
+VALUES (114, '2025-07-30T00:00:00', '2025-07-30T00:05:00', 114, 'FAIL', '192.168.0.14', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (15, '2025-07-31T00:00:00', '2025-07-31T00:02:00', 15, 'COMPLETED', '192.168.0.15', NOW());
+VALUES (115, '2025-07-31T00:00:00', '2025-07-31T00:02:00', 115, 'COMPLETED', '192.168.0.15', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (16, '2025-08-01T00:00:00', '2025-08-01T00:03:00', 16, 'SKIPPED', '192.168.0.16', NOW());
+VALUES (116, '2025-08-01T00:00:00', '2025-08-01T00:03:00', 116, 'SKIPPED', '192.168.0.16', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (17, '2025-08-02T00:00:00', '2025-08-02T00:04:00', 17, 'SKIPPED', '192.168.0.17', NOW());
+VALUES (117, '2025-08-02T00:00:00', '2025-08-02T00:04:00', 117, 'SKIPPED', '192.168.0.17', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (18, '2025-08-03T00:00:00', '2025-08-03T00:05:00', 18, 'IN_PROGRESS', '192.168.0.18', NOW());
+VALUES (118, '2025-08-03T00:00:00', '2025-08-03T00:05:00', 118, 'IN_PROGRESS', '192.168.0.18', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (19, '2025-08-04T00:00:00', '2025-08-04T00:05:00', 19, 'FAIL', '192.168.0.19', NOW());
+VALUES (119, '2025-08-04T00:00:00', '2025-08-04T00:05:00', 119, 'FAIL', '192.168.0.19', NOW());
 INSERT INTO backups (id, created_at, ended_at, metadata_id, status, worker, updated_at)
-VALUES (20, '2025-08-05T00:00:00', '2025-08-05T00:02:00', 20, 'SKIPPED', '192.168.0.20', NOW());
+VALUES (120, '2025-08-05T00:00:00', '2025-08-05T00:02:00', 120, 'SKIPPED', '192.168.0.20', NOW());
